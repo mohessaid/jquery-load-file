c4ffac1673cb7c4204eb7ae5e80c18c0ecaa8676
Mohammed Essaid MEZERREG <moh.essaid@gmail.com>
2016-12-18 12:45:06 +0000

    add gif and show, hide

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
        
        $(document).on("click", "#loadcontent", function(){
           console.log("hello");
-          $(document).load( "content/main.html", function(resp, status, xhr){
+          $(document).load( "pages/main.html", function(resp, status, xhr){
+              $("#loading").show();
               if (status == "success" && xhr.status == 200)
                 $("#mainpage-load").prepend(resp);
               else{
                   console.log("something wrong happend!");
               }
+              $("#loading").hide();
           });
            
        });
